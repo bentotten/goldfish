066a4ca48473f97ac09874a98f774d6492f0bc7c
bentotten <59932872+bentotten@users.noreply.github.com>
2020-11-21 15:14:51 -0800

    Basic Server page

--- src/Server.js
+++ src/Server.js
@@ -1,1 +1,10 @@
-//LMAO
+const http = require('https');
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('<h2>Hello World, Node!</h2>');
+  res.end();
+});
+
+server.listen(${port}, () => console.log('Server running...'));
