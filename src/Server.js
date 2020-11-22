450785d7014ed8d1a912ca87f110aed5ab77f8fc
Ben Totten <ttotten@pdx.edu>
2020-11-22 15:07:21 -0800

    Fixed some formatting issues

--- src/Server.js
+++ src/Server.js
@@ -16,6 +16,7 @@
 const repo = 'https://github.com/bentotten/goldfish.git'
 
 // Config file and scripts to install on the new instance
+/******  IMPORTANT! DO NOT FORMAT THESE LINES! CONFIG FILE CANNOT READ THE WHITE SPACE!   *********/
 const config = {
   os: 'ubuntu',
   http: true,
@@ -24,73 +25,73 @@ const config = {
       {
         key: 'startup-script',
         value: `#! /bin/bash
-        echo "Startup Started" > /var/www/log.txt
-        export HOME=/root
-        echo "export HOME=/root" >> /var/www/log.txt
+echo "Startup Started" > /var/www/log.txt
+export HOME=/root
+echo "export HOME=/root" >> /var/www/log.txt
 
-        apt-get update
-        apt-get install -y inotify-tools tmux git nginx build-essential supervisor npm
-        echo "installed dependencies" >> /var/www/log.txt
+apt-get update
+apt-get install -y inotify-tools tmux git nginx build-essential supervisor npm
+echo "installed dependencies" >> /var/www/log.txt
 
-        mkdir /var/www
+mkdir /var/www
 
-        apt-get -y upgrade
-        echo "Startup-Ran" >> /var/www/log.txt
+apt-get -y upgrade
+echo "Startup-Ran" >> /var/www/log.txt
 
-        /var/www/goldfish/backend/deployment.sh
-        #Log
-        echo "Starting Deployment" >>/var/www/log.txt
+#/var/www/goldfish/backend/deployment.sh
+# Log
+echo "Starting Deployment" >>/var/www/log.txt
 
-        # Go to proper dir
-        cd /var/www/
+# Go to proper dir
+cd /var/www/
 
-        # Install nodejs
-        mkdir /var/www/nodejs
-        curl https://nodejs.org/dist/v8.12.0/node-v8.12.0-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
-        ln -s /var/www/nodejs/bin/node /usr/bin/node
-        ln -s /var/www/nodejs/bin/npm /usr/bin/npm
+# Install nodejs
+mkdir /var/www/nodejs
+curl https://nodejs.org/dist/v8.12.0/node-v8.12.0-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
+ln -s /var/www/nodejs/bin/node /usr/bin/node
+ln -s /var/www/nodejs/bin/npm /usr/bin/npm
 
-        echo "Installed nodejs" >>/var/www/log.txt
+echo "Installed nodejs" >>/var/www/log.txt
 
-        # Create a nodeapp user. The application will run as this user.
-        useradd -m -d /home/nodeapp nodeapp
-        chown -R nodeapp:nodeapp /opt/app
-        USER = 'nodeapp'
+# Create a nodeapp user. The application will run as this user.
+useradd -m -d /home/nodeapp nodeapp
+chown -R nodeapp:nodeapp /opt/app
+USER = 'nodeapp'
 
-        echo "created nodeapp user" >>/var/www/log.txt
+echo "created nodeapp user" >>/var/www/log.txt
 
-        # Fix NPM's issues
-        npm cache clean -f
-        npm install -g n
-        n stable
-        echo "Installed fresh npm" >>/var/www/log.txt
+# Fix NPM's issues
+npm cache clean -f
+npm install -g n
+n stable
+echo "Installed fresh npm" >>/var/www/log.txt
 
-        # git repo and install dependencies
-        git config --global credential.helper gcloud.sh
-        # Clone repo and then install npm dependencies. && prevents async install of npm dep before repo is installed
-        git -C /var/www clone ${repo} && npm i --prefix /var/www/goldfish
-        echo "cloned repo" >> /var/www/log.txt
+# git repo and install dependencies
+git config --global credential.helper gcloud.sh
+# Clone repo and then install npm dependencies. && prevents async install of npm dep before repo is installed
+git -C /var/www clone ${repo}
+echo "cloned repo" >> /var/www/log.txt
 
-        #npm i --prefix /var/www/goldfish
-        npm audit fix --prefix /var/www/goldfish
-        npm run build --prefix /var/www/goldfish
-        echo "website built" >>/var/www/log.txt
+npm i --prefix /var/www/goldfish
+npm audit fix --prefix /var/www/goldfish
+npm run build --prefix /var/www/goldfish
+echo "website built" >>/var/www/log.txt
 
-        # Setup nginx
-        cat <<EOF >/etc/nginx/sites-available/default
-        server {
-            listen 80 default_server;
-           root /var/www/goldfish/build;
-           server_name http://fullstack-project-goldfish.ipq.co/;
-           index index.html index.htm;
-           location /files/ {
-               autoindex on;
-               root /var/www/goldfish/;
-            }
-          }
-        EOF
+# Setup nginx
+cat <<EOF >/etc/nginx/sites-available/default
+server {
+  listen 80 default_server;
+  root /var/www/goldfish/build;
+  server_name http://fullstack-project-goldfish.ipq.co/;
+  index index.html index.htm;
+  location /files/ {
+  autoindex on;
+  root /var/www/goldfish/;
+  }
+}
+EOF
 
-# IMPORTANT! DO NOT FORMAT THESE LINES! CONFIG FILE CANNOT READ THE WHITE SPACE!
+# Setup supervisor config
         cat > /etc/supervisor/conf.d/node-app.conf <<EOF
 [program:nginx]
 command=/usr/sbin/nginx -g "daemon off;"
@@ -107,25 +108,25 @@ stdout_logfile=/var/log/supervisor/%(program_name)s_stdout.log
 stdout_logfile_maxbytes=10MB
 EOF
 
-        supervisorctl reread
-        supervisorctl update
-        echo $(supervisorctl) >> /var/www/log.txt
-        echo "Supervisor created and launched" >>/var/www/log.txt
+supervisorctl reread
+supervisorctl update
+echo $(supervisorctl) >> /var/www/log.txt
+echo "Supervisor created and launched" >>/var/www/log.txt
 
-        echo "deployment-Ran" >>/var/www/log.txt
+echo "deployment-Ran" >>/var/www/log.txt
 
-        echo "Starting firewall rules" >>/var/www/log.txt
+echo "Starting firewall rules" >>/var/www/log.txt
 
-        gcloud compute firewall-rules create default-allow-http-8080 \
-        --allow tcp:8080 \
-        --source-ranges 0.0.0.0/0 \
-        --target-tags http-server \
-        --description "Allow port 8080 access to http-server"
+gcloud compute firewall-rules create default-allow-http-8080 \
+--allow tcp:8080 \
+--source-ranges 0.0.0.0/0 \
+--target-tags http-server \
+--description "Allow port 8080 access to http-server"
 
-        echo "gcloud-Ran" >> /var/www/log.txt
+echo "gcloud-Ran" >> /var/www/log.txt
 
-        echo "Done" >>/var/www/log.txt
-        `,
+echo "Done" >>/var/www/log.txt
+`,
       },
     ],
   },
