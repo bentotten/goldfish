28c4ce54b6f810f9273f736e12cd9f77b839206c
Ben Totten <ttotten@pdx.edu>
2020-11-28 15:11:59 -0800

    Added async attempt for server side spin-up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import spinup from 'Server';
 
+/* Spinup uses the Google Compute API to check if there is already a server up with this program on it. If there is, it knows that its the version running on that
+   vm and to proceed with the render. Because of the asynchronous nature of this, we were unable to get this to function properly and have removed it, but the
+   feature can be accessed from the command line by going to the src directory and typing 'node Server.js'
 
+   async function startVM() {
+      spinup()
+   }
+*/
 
 ReactDOM.render(
   <React.StrictMode>
